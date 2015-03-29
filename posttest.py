1c626b86874b32e7e3e9fb88e796a32c4ca207f3
Andy <aleeodm25@gmail.com>
2015-03-29 01:06:04 -0400

    first update to accomidate xbee

--- posttest.py
+++ posttest.py
@@ -1,75 +1,62 @@
 import json, datetime, requests, ast, fnmatch
-import os, time
-from w1_therm import w1_therm
+import os, serial, time
 #endpoints
 #.../tempsensor				get/post
 #.../tempsensor/(serial#)	get/put/delete
 #.../temperature/(serial#)	get/post
 base_url = "http://192.168.1.131:9000/api/"
-device_dir = '/sys/bus/w1/devices/'
 headers = {'content-type': 'application/json'}
-def addtodb(sensor):
+ser = serial.Serial('/dev/ttyAmA0',9600, timeout=0, writeTimeout=0)
+
+def addsensortodb(sensor):
 	url = base_url+"tempsensor"
 	payload = {}
 	payload['serial'] = sensor
 	return requests.post(url, data=json.dumps(payload), headers = headers)
 
-def updatedbstatus(sensor, status):
-	url = base_url+"tempsensor/"+sensor
-	payload = {'serial':sensor,'active':status}
-	return requests.put(url, data=json.dumps(payload), headers = headers)
-#get attatched sensors
-atsensors = []
-for filename in os.listdir("/sys/bus/w1/devices"):
-	if fnmatch.fnmatch(filename, '28-*'):
-		atsensors.append(filename)
-print("active sensors: ",atsensors)
+def addtemptodb(serial, temp):
+    url = base_url+"temperature/"+serial
+    payload = {'serial':serial, 'temperature':round(temp,3), 'timestamp':str(datetime.datetime.now())}
+    r = requests.post(url, data=json.dumps(payload), headers={'content-type': 'application/json'}
+
 
 #get current devices in database
 r = requests.get(base_url+"tempsensor")
 temp = json.dumps(r.json())
-temp = temp.replace('true','True')
-temp = temp.replace('false','False')
 dict_list = ast.literal_eval(temp)
-dbsensors = {}
-
-#transform database data into proper form of {"serial":"status"}
+dbsensors = []
 for dict in dict_list:
-	dbsensors[dict['serial']] = dict['active']
-print("List of sensors in database: ", dbsensors)
-changelist = {}
-for sensor in atsensors:
-	if sensor in dbsensors: 
-		if dbsensors[sensor] == False:
-			updatedbstatus(sensor,True)
-			changelist[sensor] = "update status from False to True"
-	elif sensor not in dbsensors:
-		addtodb(sensor)
-		changelist[sensor] = "add to database"
-
-for sensor in dbsensors:
-	if dbsensors[sensor] == True:
-		if sensor not in atsensors:
-			updatedbstatus(sensor,False)
-			changelist[sensor] = "update status from True to False"
-print("List of changes: ",changelist)
+    dbsensors.append(dict['serial'])
+print("Sensors in database: ",dbsensors)
 
 
-#read temperature sensors 
-for sensor in atsensors:
-	sobj = w1_therm(sensor)
-	temp = sobj.get_temp_f()[0]
-	url = base_url+"temperature/"+sensor
-	payload = {'serial':sensor, 'temperature':round(temp,3), 'timestamp':str(datetime.datetime.now())} 
-	r = requests.post(url, data=json.dumps(payload), headers=headers)
+#xbee read push loop
+while True:
+    #wait until full packet(dont know why 21 bytes)
+    if ser.inWaiting() >= 21:
+        #ord() converts a char byte into int()
+        firstbyte = ord(ser.read(1))
+        #value packets start with 0x7e
+        if firstbyte == 0x7e:
+            lengthhi = ser.read(1)
+            lengthlo = ser.read(1)
+            frametype = ser.read(1)
+            sourcemac = ser.read(8)
+            serialnum = ""
+            for i in sourcemac:
+                serialnum += hex(ord(i))[2:]
 
-#new temperature 
-#url = "http://192.168.1.14:9000/api/temperature/28-0215684fd5ff"
-#payload = {'serial': '28-0214684fd5ff', 'temperature': 123.456, 'timestamp': str(datetime.datetime.now())}
-#headers = {'content-type': 'application/json'}
-#print(payload)
-#r = requests.post(url, data=json.dumps(payload), headers=headers)
-#print(r.response)
+            if serialnum not in dbsensors:
+                addsensortodb(serialnum)
+                print("added %s to databse" %serialnum)
 
-#r = requests.delete(url+"/28-0214684fd5ff")
-#print(r.status_code)
+            sourceethi = ser.read(1)
+            sourcenetlo = ser.read(1)a
+            junk = ser.read(5)
+            volthi = ord(ser.read(1))
+            voltlo = ord(ser.read(1))
+            volt = voltlo+(volthi(*256)
+            tempc = ((volt/1023.*1.2 -.5)*100)
+            tempf = tempc*(9/5.)+32
+            addtemptodb(serialnum, tempf)
+
