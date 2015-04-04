b61995a5dfc74313e5ef74c124b75054bd3b9ac8
Andy <aleeodm25@gmail.com>
2015-04-03 20:19:32 -0400

    add unicode fields

--- main/models.py
+++ main/models.py
@@ -3,17 +3,21 @@ from django.db import models
 	
 class Room(models.Model):
 	name = models.CharField(max_length=50)
-        
         #historical high and low temps
         temperature_high = models.OneToOneField('Temperature', null=True, default=None, related_name='+')
         temperature_low = models.OneToOneField('Temperature', null=True, default=None, related_name='+')
-
         #current temperature
         temperature_current = models.OneToOneField('Temperature', null=True, default=None, related_name='+')
+        
+        def _unicode__(self):
+            return self.name
 
 class Tsensor(models.Model):
 	serial = models.CharField(max_length=14,primary_key=True)
         room = models.ForeignKey('Room', null=True, default=None)
+        
+        def __unicode__(self):
+            return self.serial
 
 class Temperature(models.Model):
 	serial = models.ForeignKey('Tsensor')
