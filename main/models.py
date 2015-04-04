f2d617972c5cbfcac03a129ef409a7f298c9ee8f
Andy <aleeodm25@gmail.com>
2015-04-04 18:46:51 -0400

    re-strucutre data model

--- main/models.py
+++ main/models.py
@@ -4,22 +4,23 @@ from django.db import models
 class Room(models.Model):
 	name = models.CharField(max_length=50)
         #historical high and low temps
-        temperature_high = models.OneToOneField('Temperature',blank=True ,null=True, default=None, related_name='+')
-        temperature_low = models.OneToOneField('Temperature',blank=True, null=True, default=None, related_name='+')
-        #current temperature
-        temperature_current = models.OneToOneField('Temperature',blank=True, null=True, default=None, related_name='+')
-        
-        sensor = models.ForeignKey('Tsensor',blank=True, null=True, default=None)
+        temperature_high = models.OneToOneField('Data',blank=True ,null=True, default=None, related_name='+')
+        temperature_low = models.OneToOneField('Data',blank=True, null=True, default=None, related_name='+')
+        #current data
+        temperature_current = models.OneToOneField('Data',blank=True, null=True, default=None, related_name='+')
+        humidity_current = models.OneToOneField('Data', blank=True, null=True, default=None, related_name='+')
+        vent_state = models.IntegerField(null=True,default=None)
         def _unicode__(self):
             return self.name
 
-class Tsensor(models.Model):
+class Sensor(models.Model):
 	serial = models.CharField(max_length=14,primary_key=True)
-        
+        room_id = models.ForeignKey('Room')
         def __unicode__(self):
             return self.serial
 
-class Temperature(models.Model):
-	serial = models.ForeignKey('Tsensor')
-	temperature = models.DecimalField(max_digits=6,decimal_places=3)
-	timestamp = models.DateTimeField()
+class Data(models.Model):
+	room_id = models.ForeignKey('Room')
+	data = models.DecimalField(max_digits=6,decimal_places=3)
+	time = models.DateTimeField()
+        data_type = models.IntegerField(null=True, default=1)
