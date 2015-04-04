ac70a86dbb6658148617d4a8f5fe7efe3dde1337
Andy <aleeodm25@gmail.com>
2015-04-03 20:07:48 -0400

    fix errors in main/models.py

--- main/models.py
+++ main/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
+	
 class Room(models.Model):
 	name = models.CharField(max_length=50)
         
         #historical high and low temps
-        temperature_high = models.ForeignKey(Temperature, blank=True, default=blank)
-        temperature_low = models.ForeignKey(Temperature, blank=True, default=blank)
+        temperature_high = models.OneToOneField('Temperature', null=True, default=None, related_name='+')
+        temperature_low = models.OneToOneField('Temperature', null=True, default=None, related_name='+')
 
         #current temperature
-        temperature_current = models.ForeignKey(Temperature, blank=True, default=blank)
+        temperature_current = models.OneToOneField('Temperature', null=True, default=None, related_name='+')
 
 class Tsensor(models.Model):
 	serial = models.CharField(max_length=14,primary_key=True)
-        room = models.ForeignKey(Room, blank=True, default=blank)
+        room = models.ForeignKey('Room', null=True, default=None)
 
 class Temperature(models.Model):
-	serial = models.ForeignKey(Tsensor)
+	serial = models.ForeignKey('Tsensor')
 	temperature = models.DecimalField(max_digits=6,decimal_places=3)
 	timestamp = models.DateTimeField()
-	
