1d52f8c0e49a67b21ba974bcd110abb3b058e2c9
Andy <aleeodm25@gmail.com>
2015-04-04 21:15:11 -0400

    sensor enpoint /api/sensor -> /api/sensors

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
 	'api.views',
-	url(r'^sensor$', 'Sensor_list', name='Sensor_list'),
-	url(r'^sensor/(?P<serial>[\w{}.-]{1,15})$', 'Sensor_detail', name='Sensor_detail'),
+	url(r'^sensors$', 'Sensor_list', name='Sensor_list'),
+	url(r'^sensors/(?P<serial>[\w{}.-]{1,15})$', 'Sensor_detail', name='Sensor_detail'),
 	url(r'^data$', 'Data_post', name='Data_post'),
 )
