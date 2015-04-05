62086753ccb8362cb5b142ba3fa19cdc7f1e30d0
root <aleeodm25@gmail.com>
2015-04-05 18:35:46 +0000

    more updates

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,12 @@
 from django import forms
 from django.forms import ModelForm
 from main.models import *
-
+"""
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = ('name',)
-
-class SensorForm(ModelForm):
-    class Meta:
-        model = Sensor
-        fields = ('room_id',)
-
+"""
+class RoomForm(ModelForm):
+    name = forms.CharField(max_length=50)
+    sensor_id = forms.ModelChoiceField(empty_label="(Nothing"), to_field_name="serial")
